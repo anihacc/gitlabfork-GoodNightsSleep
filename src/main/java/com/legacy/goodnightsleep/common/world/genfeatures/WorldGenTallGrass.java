23708fedee4844393449eda117cf0af0a5df81fe
KingPhygieBoo <wheresperry41@gmail.com>
2017-12-15 07:53:07 -0600

    Fixed the tallgrass being in too small of chunks.

--- src/main/java/com/legacy/goodnightsleep/common/world/genfeatures/WorldGenTallGrass.java
+++ src/main/java/com/legacy/goodnightsleep/common/world/genfeatures/WorldGenTallGrass.java
@@ -25,8 +25,8 @@ public class WorldGenTallGrass extends WorldGenerator
             position = position.down();
         }
 
-        //for (int i = 0; i < 128; ++i)
-        for (int i = 0; i < 50; ++i)
+        for (int i = 0; i < 128; ++i)
+        //for (int i = 0; i < 50; ++i)
         {
             BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
