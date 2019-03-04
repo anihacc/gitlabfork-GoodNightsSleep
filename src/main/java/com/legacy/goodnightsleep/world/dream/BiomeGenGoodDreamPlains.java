74af15e5e5872c0ee43a27b8cd7db34d7a34d5dc
Bailey <wheresperry41@gmail.com>
2019-03-04 16:26:30 -0600

    Updated Dream water color

--- src/main/java/com/legacy/goodnightsleep/world/dream/BiomeGenGoodDreamPlains.java
+++ src/main/java/com/legacy/goodnightsleep/world/dream/BiomeGenGoodDreamPlains.java
@@ -16,21 +16,10 @@ public class BiomeGenGoodDreamPlains extends Biome
 	public BiomeGenGoodDreamPlains()
 	{
 		super(new BiomeProperties("Good Dream Plains").setRainDisabled().setBaseHeight(0.1F).setHeightVariation(0.5F).setTemperature(0.8F).setRainfall(0.0F));
-		this.spawnableMonsterList.clear(); //.setRainDisabled() .setRainfall(0.0F)
+		this.spawnableMonsterList.clear();
 
 		this.spawnableCreatureList.clear();
 		this.spawnableCreatureList.add(new Biome.SpawnListEntry(EntityGNSSpawner.class, 100, 1, 1));
-		
-		//ArrayList<SpawnListEntry> list = new ArrayList<SpawnListEntry>();
-
-		//this.addCreatureEntry(list);
-
-		//this.spawnableCreatureList.addAll(list);
-
-		//list.clear();
-
-		//this.topBlock = BlocksGNS.dream_grass.getDefaultState();
-		//this.fillerBlock = BlocksGNS.dream_dirt.getDefaultState();
 	}
 
 	@Override
@@ -38,25 +27,17 @@ public class BiomeGenGoodDreamPlains extends Biome
     {
     	return new WorldGenTallGrass(BlocksGNS.tall_dream_grass.getDefaultState());
     }
-	
-	/*private void addCreatureEntry(ArrayList<SpawnListEntry> list)
-	{
-		//list.add(new SpawnListEntry(EntityStardustSheep.class, 25, 3, 4));
-		list.add(new SpawnListEntry(EntityPig.class, 15, 2, 4));
-		list.add(new SpawnListEntry(EntityHorse.class, 7, 3, 4));
-		//list.add(new SpawnListEntry(EntityAzulfo.class, 27, 3, 5));
-	}*/
-	
+
 	public BiomeDecorator createBiomeDecorator()
     {
     	return new GoodDreamBiomeDecorator();
     }
 	
-	/*@SideOnly(Side.CLIENT)
-    public final int setWaterColor()
-    {
-        return 0xcee7f5;
-    }*/
+	@Override
+	public int getWaterColorMultiplier()
+	{
+		return 0x00ff97; //0x727272
+	}
 	
 	public Biome.TempCategory getTempCategory()
     {
