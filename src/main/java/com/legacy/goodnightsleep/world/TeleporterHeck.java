e668d522a9cad5831ad8f0638357cbb7a708673f
Bailey <wheresperry41@gmail.com>
2019-03-04 16:12:04 -0600

    Fixed teleporting to the dimensions and being stuck into the ground

--- src/main/java/com/legacy/goodnightsleep/world/TeleporterHeck.java
+++ src/main/java/com/legacy/goodnightsleep/world/TeleporterHeck.java
@@ -4,43 +4,41 @@ import java.util.Random;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
 public class TeleporterHeck extends Teleporter
 {
 	Random random;
-	
-    public TeleporterHeck(WorldServer worldIn)
-    {
-    	super(worldIn);
-    	
-        this.random = new Random(worldIn.getSeed());
-    }
-    
-    public void placeInPortal(Entity entityIn, float rotationYaw)
-    {
-    }
 
-    public boolean placeInExistingPortal(Entity entityIn, float rotationYaw)
-    {
+	public TeleporterHeck(WorldServer worldIn)
+	{
+		super(worldIn);
+
+		this.random = new Random(worldIn.getSeed());
+	}
+
+	@Override
+	public void placeInPortal(Entity entityIn, float rotationYaw)
+	{
+		int i = MathHelper.floor(entityIn.posX);
+		int k = MathHelper.floor(entityIn.posZ);
+        entityIn.setLocationAndAngles((double)i, this.world.getTopSolidOrLiquidBlock(new BlockPos(i, 0, k)).getY(), (double)k, entityIn.rotationYaw, 0.0F);
+		entityIn.motionX = 0.0D;
+		entityIn.motionY = 0.0D;
+		entityIn.motionZ = 0.0D;
+	}
+
+	@Override
+	public boolean placeInExistingPortal(Entity entityIn, float rotationYaw)
+	{
 		return false;
-    }
+	}
 
-    public void removeStalePortalLocations(long worldTime)
-    {
-    }
-
-    public class PortalPosition extends BlockPos
-    {
-        /** The worldtime at which this PortalPosition was last verified */
-        public long lastUpdateTime;
-
-        public PortalPosition(BlockPos pos, long lastUpdate)
-        {
-            super(pos.getX(), pos.getY(), pos.getZ());
-            this.lastUpdateTime = lastUpdate;
-        }
-    }
-
+	@Override
+	public boolean makePortal(Entity entityIn)
+	{
+		return false;
+	}
 }
