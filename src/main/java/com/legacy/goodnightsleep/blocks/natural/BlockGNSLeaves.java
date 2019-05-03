e044807ac1b3236dc87dee1ca0dd6dfff3a972e5
Bailey <wheresperry41@gmail.com>
2019-05-03 01:54:31 -0500

    oopsie

--- src/main/java/com/legacy/goodnightsleep/blocks/natural/BlockGNSLeaves.java
+++ src/main/java/com/legacy/goodnightsleep/blocks/natural/BlockGNSLeaves.java
@@ -17,6 +17,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -123,5 +124,12 @@ public class BlockGNSLeaves extends BlockLeaves
 	{
 		return null;
 	}
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+    	return true;
+    }
 
 }
