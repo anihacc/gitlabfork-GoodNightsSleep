9ef5a31c37534ccb91a6a3273977de9e74686086
Bailey <wheresperry41@gmail.com>
2019-05-07 22:23:35 -0500

    Updated version numbers

--- src/main/java/com/legacy/goodnightsleep/registry/VariableConstants.java
+++ src/main/java/com/legacy/goodnightsleep/registry/VariableConstants.java
@@ -9,7 +9,7 @@ public class VariableConstants
 
 	public static final String NAME = "Good Night's Sleep";
 
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.1";
 
 	public static final String MODID = "goodnightsleep";
 
