252866b07710c16e3a8c1906edb549ab9dab4aae
Bailey <wheresperry41@gmail.com>
2019-07-19 20:32:05 -0500

    Nerfed Negatite sword

--- src/main/java/com/legacy/goodnightsleep/item/GNSItemTier.java
+++ src/main/java/com/legacy/goodnightsleep/item/GNSItemTier.java
@@ -13,21 +13,11 @@ public enum GNSItemTier implements IItemTier
 	CANDY(1, 196, 4.0F, 1.0F, 5, () -> {
 		return Ingredient.fromItems(BlocksGNS.candy_block);
 	}),
-	NECRUM(1, 131, 6.0F, 2.0F, 0, () -> {
-		return Ingredient.fromItems(ItemsGNS.necrum);
-	}),
-	RAINBOW(2, 375, 6.0F, 2.0F, 14, () -> {
-		return Ingredient.fromItems(ItemsGNS.rainbow_ingot);
-	}),
-	ZITRITE(3, 1561, 8.0F, 3.0F, 10, () -> {
-		return Ingredient.fromItems(ItemsGNS.zitrite_ingot);
-	}),
-	POSITITE(3, 2341, 8.0F, 3.0F, 10, () -> {
-		return Ingredient.fromItems(ItemsGNS.positite_gem);
-	}),
-	NEGATITE(4, 1561, 10.0F, 4.0F, 0, () -> {
-		return Ingredient.fromItems(ItemsGNS.negatite_gem);
-	});
+	NECRUM(1, 131, 6.0F, 2.0F, 0, () -> {return Ingredient.fromItems(ItemsGNS.necrum);}),
+	RAINBOW(2, 375, 6.0F, 2.0F, 14, () -> {return Ingredient.fromItems(ItemsGNS.rainbow_ingot);}),
+	ZITRITE(3, 1561, 8.0F, 3.0F, 10, () -> {return Ingredient.fromItems(ItemsGNS.zitrite_ingot);}),
+	POSITITE(3, 2341, 8.0F, 3.0F, 10, () -> {return Ingredient.fromItems(ItemsGNS.positite_gem);}),
+	NEGATITE(4, 1561, 10.0F, 3.5F, 0, () -> {return Ingredient.fromItems(ItemsGNS.negatite_gem);});
 
 	private final int harvestLevel;
 
