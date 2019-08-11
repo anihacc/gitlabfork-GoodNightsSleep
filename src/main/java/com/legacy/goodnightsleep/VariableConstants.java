eb29a854e629ab73ae7f0025b5129a95275e3733
Lachney <jonlachney@gmail.com>
2019-08-11 11:55:58 -0500

    0.0.2

--- src/main/java/com/legacy/goodnightsleep/VariableConstants.java
+++ src/main/java/com/legacy/goodnightsleep/VariableConstants.java
@@ -7,7 +7,7 @@ public class VariableConstants
 
 	public static final String NAME = "Good Night's Sleep";
 
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 
 	public static final String MODID = "goodnightsleep";
 
