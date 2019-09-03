63a76a1236d5058762a7d4580fc8431e76af5046
KingPhygieBoo <wheresperry41@gmail.com>
2019-09-03 02:49:28 -0500

    Fixed a typo within the foods

--- src/main/java/com/legacy/goodnightsleep/item/GNSFoods.java
+++ src/main/java/com/legacy/goodnightsleep/item/GNSFoods.java
@@ -5,9 +5,9 @@ import net.minecraft.item.Food;
 public class GNSFoods
 {
 
-	public static final Food CANDY = (new Food.Builder()).hunger(4).saturation(0.6F).fastToEat().build();
+	public static final Food RAINBOW_BERRIES = (new Food.Builder()).hunger(6).saturation(0.6F).build();
 
-	public static final Food RAINBOW_BERRIES = (new Food.Builder()).hunger(2).saturation(0.2F).fastToEat().build();
+	public static final Food CANDY = (new Food.Builder()).hunger(2).saturation(0.2F).fastToEat().build();
 	
 	public static final Food TELEPORTATION_STEW = (new Food.Builder()).hunger(0).saturation(0.0F).setAlwaysEdible().build();
 }
