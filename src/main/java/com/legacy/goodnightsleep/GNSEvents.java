769688c709bc654af33de787ceaa2e8056c016f7
KingPhygieBoo <wheresperry41@gmail.com>
2019-12-29 10:57:03 -0600

    Adjusted tilling

--- src/main/java/com/legacy/goodnightsleep/GNSEvents.java
+++ src/main/java/com/legacy/goodnightsleep/GNSEvents.java
@@ -8,11 +8,13 @@ import com.legacy.goodnightsleep.world.GNSDimensions;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -22,7 +24,6 @@ import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -30,6 +31,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class GNSEvents
 {
 	public static final Map<Block, Block> BLOCK_STRIPPING_MAP = (new Builder<Block, Block>()).put(GNSBlocks.dream_log, GNSBlocks.stripped_dream_log).put(GNSBlocks.dream_wood, GNSBlocks.stripped_dream_wood).put(GNSBlocks.white_log, GNSBlocks.stripped_white_log).put(GNSBlocks.white_wood, GNSBlocks.stripped_white_wood).put(GNSBlocks.dead_log, GNSBlocks.stripped_dead_log).put(GNSBlocks.dead_wood, GNSBlocks.stripped_dead_wood).put(GNSBlocks.blood_log, GNSBlocks.stripped_blood_log).put(GNSBlocks.blood_wood, GNSBlocks.stripped_blood_wood).build();
+	public static final Map<Block, BlockState> BLOCK_TILL_MAP = (new Builder<Block, BlockState>()).put(GNSBlocks.dream_dirt, GNSBlocks.dream_farmland.getDefaultState()).put(GNSBlocks.nightmare_grass_block, Blocks.FARMLAND.getDefaultState()).build();
 
 	public PlayerEntity player;
 
@@ -50,19 +52,8 @@ public class GNSEvents
 	{
 		World world = event.getWorld();
 		BlockPos pos = event.getPos();
-		BlockState state = world.getBlockState(pos);
 		PlayerEntity player = event.getPlayer();
 		ItemStack stack = event.getItemStack();
-
-		if ((stack.getItem() instanceof HoeItem) && world.isAirBlock(pos.up()))
-		{
-			Block block = state.getBlock();
-
-			if (block == GNSBlocks.dream_grass_block || block == GNSBlocks.dream_dirt)
-			{
-				hoeDirt(stack, player, world, pos, GNSBlocks.dream_farmland.getDefaultState(), event);
-			}
-		}
 
 		if (stack.getItem() instanceof AxeItem)
 		{
@@ -87,30 +78,29 @@ public class GNSEvents
 				}
 			}
 		}
-	}
 
-	protected void hoeDirt(ItemStack stack, PlayerEntity player, World worldIn, BlockPos pos, BlockState state, PlayerInteractEvent.RightClickBlock event)
-	{
-		worldIn.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-		if (!worldIn.isRemote)
+		if (event.getItemStack().getItem() instanceof HoeItem)
 		{
-			worldIn.setBlockState(pos, state, 11);
-			stack.damageItem(1, player, (p_219991_1_) ->
+			BlockState dirtState = world.getBlockState(pos);
+			BlockState resultState = BLOCK_TILL_MAP.get(dirtState.getBlock());
+
+			if (resultState != null && event.getFace() != Direction.DOWN && world.isAirBlock(pos.up()))
 			{
-				p_219991_1_.sendBreakAnimation(player.getActiveHand());
-			});
-		}
-		player.swingArm(event.getHand());
-	}
-
-	protected void plantSeed(ItemStack stack, PlayerEntity player, World worldIn, BlockPos pos, BlockState state)
-	{
-		if (!worldIn.isRemote)
-		{
-			worldIn.setBlockState(pos, state, 11);
-
-			if (!player.isCreative())
-				stack.shrink(1);
+				player.getEntityWorld().playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				player.swingArm(event.getHand());
+				if (!world.isRemote)
+				{
+					world.setBlockState(pos, resultState, 11);
+					if (player != null)
+					{
+						event.getItemStack().damageItem(1, player, (playerEntity) ->
+						{
+							playerEntity.sendBreakAnimation(event.getHand());
+						});
+					}
+				}
+			}
+			event.setCanceled(true);
 		}
 	}
 
