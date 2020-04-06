aa4803ec8e278d31cc413956bfc9d32b44ffc341
Bailey <wheresperry41@gmail.com>
2020-04-05 23:17:41 -0500

    Axe stripping and dirt tilling work differently now

--- src/main/java/com/legacy/goodnightsleep/GNSEvents.java
+++ src/main/java/com/legacy/goodnightsleep/GNSEvents.java
@@ -1,26 +1,11 @@
 package com.legacy.goodnightsleep;
 
-import java.util.Map;
-
-import com.google.common.collect.ImmutableMap.Builder;
-import com.legacy.goodnightsleep.blocks.GNSBlocks;
 import com.legacy.goodnightsleep.world.GNSDimensions;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.entity.passive.horse.AbstractHorseEntity;
 import net.minecraft.entity.passive.horse.SkeletonHorseEntity;
 import net.minecraft.entity.passive.horse.ZombieHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.AxeItem;
-import net.minecraft.item.HoeItem;
-import net.minecraft.util.Direction;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
@@ -28,16 +13,11 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event.Result;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class GNSEvents
 {
-	public static final Map<Block, Block> BLOCK_STRIPPING_MAP = (new Builder<Block, Block>()).put(GNSBlocks.dream_log, GNSBlocks.stripped_dream_log).put(GNSBlocks.dream_wood, GNSBlocks.stripped_dream_wood).put(GNSBlocks.white_log, GNSBlocks.stripped_white_log).put(GNSBlocks.white_wood, GNSBlocks.stripped_white_wood).put(GNSBlocks.dead_log, GNSBlocks.stripped_dead_log).put(GNSBlocks.dead_wood, GNSBlocks.stripped_dead_wood).put(GNSBlocks.blood_log, GNSBlocks.stripped_blood_log).put(GNSBlocks.blood_wood, GNSBlocks.stripped_blood_wood).build();
-	public static final Map<Block, BlockState> BLOCK_TILL_MAP = (new Builder<Block, BlockState>()).put(GNSBlocks.dream_grass_block, GNSBlocks.dream_farmland.getDefaultState()).put(GNSBlocks.dream_dirt, GNSBlocks.dream_farmland.getDefaultState()).put(GNSBlocks.nightmare_grass_block, Blocks.FARMLAND.getDefaultState()).build();
-
-	public PlayerEntity player;
-
 	@SubscribeEvent
 	public void onRegisteredDimension(RegisterDimensionsEvent event)
 	{
@@ -56,63 +36,6 @@ public class GNSEvents
 				event.setResult(Result.ALLOW);
 				player.startRiding(event.getTarget());
 			}
-		}
-	}
-
-	@SubscribeEvent
-	public void onPlayerRightClickItem(PlayerInteractEvent.RightClickItem event)
-	{
-		PlayerEntity player = event.getPlayer();
-		World world = event.getWorld();
-		BlockRayTraceResult rayTraceResult = (BlockRayTraceResult) rayTrace(world, player);
-		BlockPos pos = rayTraceResult.getPos();
-
-		if (event.getItemStack().getItem() instanceof AxeItem)
-		{
-			BlockState blockState = world.getBlockState(pos);
-			Block block = BLOCK_STRIPPING_MAP.get(blockState.getBlock());
-
-			if (block != null)
-			{
-				player.getEntityWorld().playSound(player, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
-				player.swingArm(event.getHand());
-				if (!world.isRemote)
-				{
-					world.setBlockState(pos, block.getDefaultState().with(RotatedPillarBlock.AXIS, blockState.get(RotatedPillarBlock.AXIS)), 11);
-					if (player != null)
-					{
-						event.getItemStack().damageItem(1, player, (playerEntity) ->
-						{
-							playerEntity.sendBreakAnimation(event.getHand());
-						});
-					}
-				}
-			}
-			event.setCanceled(true);
-		}
-
-		if (event.getItemStack().getItem() instanceof HoeItem)
-		{
-			BlockState dirtState = world.getBlockState(pos);
-			BlockState resultState = BLOCK_TILL_MAP.get(dirtState.getBlock());
-
-			if (resultState != null && event.getFace() != Direction.DOWN && world.isAirBlock(pos.up()))
-			{
-				player.getEntityWorld().playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-				player.swingArm(event.getHand());
-				if (!world.isRemote)
-				{
-					world.setBlockState(pos, resultState, 11);
-					if (player != null)
-					{
-						event.getItemStack().damageItem(1, player, (playerEntity) ->
-						{
-							playerEntity.sendBreakAnimation(event.getHand());
-						});
-					}
-				}
-			}
-			event.setCanceled(true);
 		}
 	}
 
---
+++ src/main/java/com/legacy/goodnightsleep/blocks/util/ToolCompat.java
@@ -0,0 +1,40 @@
+package com.legacy.goodnightsleep.blocks.util;
+
+import com.google.common.collect.Maps;
+import com.legacy.goodnightsleep.blocks.GNSBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
+
+public class ToolCompat
+{
+	public static void init()
+	{
+		axeStripping(GNSBlocks.dream_log, GNSBlocks.stripped_dream_log);
+		axeStripping(GNSBlocks.white_log, GNSBlocks.stripped_white_log);
+		axeStripping(GNSBlocks.dead_log, GNSBlocks.stripped_dead_log);
+		axeStripping(GNSBlocks.blood_log, GNSBlocks.stripped_blood_log);
+		axeStripping(GNSBlocks.dream_wood, GNSBlocks.stripped_dream_wood);
+		axeStripping(GNSBlocks.white_wood, GNSBlocks.stripped_white_wood);
+		axeStripping(GNSBlocks.dead_wood, GNSBlocks.stripped_dead_wood);
+		axeStripping(GNSBlocks.blood_wood, GNSBlocks.stripped_blood_wood);
+
+		hoeTilling(GNSBlocks.dream_grass_block, GNSBlocks.dream_farmland);
+		hoeTilling(GNSBlocks.dream_dirt, GNSBlocks.dream_farmland);
+		hoeTilling(GNSBlocks.nightmare_grass_block, Blocks.FARMLAND);
+	}
+
+	static void axeStripping(Block log, Block stripped)
+	{
+		AxeItem.BLOCK_STRIPPING_MAP = Maps.newHashMap(AxeItem.BLOCK_STRIPPING_MAP);
+		AxeItem.BLOCK_STRIPPING_MAP.put(log, stripped);
+	}
+
+	static void hoeTilling(Block dirt, Block farmland)
+	{
+		HoeItem.HOE_LOOKUP = Maps.newHashMap(HoeItem.HOE_LOOKUP);
+		HoeItem.HOE_LOOKUP.put(dirt, farmland.getDefaultState());
+	}
+}
