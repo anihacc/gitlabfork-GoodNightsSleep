bd5112d1a7b751cc9a461adf25538a51aeddd611
Bailey <wheresperry41@gmail.com>
2020-09-01 22:12:35 -0500

    ancient legacy code remnant

--- src/main/java/com/legacy/goodnightsleep/GoodNightSleep.java
+++ src/main/java/com/legacy/goodnightsleep/GoodNightSleep.java
@@ -4,10 +4,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.legacy.goodnightsleep.blocks.util.ToolCompat;
-import com.legacy.goodnightsleep.client.GoodNightSleepClient;
+import com.legacy.goodnightsleep.client.ClientEvents;
+import com.legacy.goodnightsleep.client.audio.GNSMusicHandler;
+import com.legacy.goodnightsleep.client.render.GNSEntityRendering;
+import com.legacy.goodnightsleep.client.render.GNSTileEntityRendering;
 import com.legacy.goodnightsleep.data.GNSMappingChanges;
+import com.legacy.goodnightsleep.registry.GNSBlocks;
 import com.legacy.goodnightsleep.registry.GNSFeatures;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,6 +22,7 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -26,29 +34,6 @@ public class GoodNightSleep
 	public static final String NAME = "Good Night's Sleep";
 	public static final String MODID = "good_nights_sleep";
 	public static final String OLD_MODID = "goodnightsleep";
-
-	public GoodNightSleep()
-	{
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, GNSConfig.SERVER_SPEC);
-
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::initialization);
-
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
-		{
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(GoodNightSleepClient::initialization);
-			MinecraftForge.EVENT_BUS.register(new GoodNightSleepClient());
-		});
-	}
-
-	private void initialization(final FMLCommonSetupEvent event)
-	{
-		ForgeRegistries.BIOMES.getValues().forEach(GNSFeatures::addMushrooms);
-
-		MinecraftForge.EVENT_BUS.register(new GNSMappingChanges());
-		MinecraftForge.EVENT_BUS.register(new GNSEvents());
-
-		ToolCompat.init();
-	}
 
 	public static ResourceLocation locate(String name)
 	{
@@ -63,5 +48,71 @@ public class GoodNightSleep
 	public static String find(String name)
 	{
 		return MODID + ":" + name;
+	}
+
+	public GoodNightSleep()
+	{
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, GNSConfig.SERVER_SPEC);
+
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonInit);
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientInit));
+	}
+
+	private void commonInit(final FMLCommonSetupEvent event)
+	{
+		ForgeRegistries.BIOMES.getValues().forEach(GNSFeatures::addMushrooms);
+
+		MinecraftForge.EVENT_BUS.register(new GNSMappingChanges());
+		MinecraftForge.EVENT_BUS.register(new GNSEvents());
+
+		ToolCompat.init();
+	}
+
+	public void clientInit(FMLClientSetupEvent event)
+	{
+		MinecraftForge.EVENT_BUS.register(new GNSMusicHandler());
+		MinecraftForge.EVENT_BUS.register(new ClientEvents());
+
+		GNSTileEntityRendering.initialization();
+		GNSEntityRendering.initialization();
+
+		renderCutout(GNSBlocks.candy_sapling);
+		renderCutout(GNSBlocks.dream_sapling);
+		renderCutout(GNSBlocks.cyan_flower);
+		renderCutout(GNSBlocks.dead_flower);
+		renderCutout(GNSBlocks.orange_flower);
+		renderCutout(GNSBlocks.lolipop_bush);
+		renderCutout(GNSBlocks.dream_grass);
+		renderCutout(GNSBlocks.nightmare_grass);
+		renderCutout(GNSBlocks.prickly_nightmare_grass);
+		renderCutout(GNSBlocks.hope_mushroom);
+		renderCutout(GNSBlocks.despair_mushroom);
+		renderCutout(GNSBlocks.rainbow_berries);
+		renderCutout(GNSBlocks.potted_candy_sapling);
+		renderCutout(GNSBlocks.potted_dream_sapling);
+		renderCutout(GNSBlocks.potted_cyan_flower);
+		renderCutout(GNSBlocks.potted_dead_flower);
+		renderCutout(GNSBlocks.potted_orange_flower);
+		renderCutout(GNSBlocks.potted_hope_mushroom);
+		renderCutout(GNSBlocks.potted_despair_mushroom);
+
+		renderCutout(GNSBlocks.dream_door);
+		renderCutout(GNSBlocks.white_door);
+		renderCutout(GNSBlocks.dead_door);
+		renderCutout(GNSBlocks.blood_door);
+		renderCutout(GNSBlocks.dream_trapdoor);
+		renderCutout(GNSBlocks.white_trapdoor);
+		renderCutout(GNSBlocks.dead_trapdoor);
+		renderCutout(GNSBlocks.blood_trapdoor);
+
+		RenderTypeLookup.setRenderLayer(GNSBlocks.rainbow, RenderType.getTranslucent());
+		RenderTypeLookup.setRenderLayer(GNSBlocks.dream_leaves, RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(GNSBlocks.candy_leaves, RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(GNSBlocks.diamond_leaves, RenderType.getCutoutMipped());
+	}
+
+	private static void renderCutout(Block block)
+	{
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
 }
