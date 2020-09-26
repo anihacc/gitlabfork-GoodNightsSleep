a3e14c02f81c6b72cb3caaf857ad820e24e2efa0
Bailey <wheresperry41@gmail.com>
2020-09-25 23:53:22 -0500

    Removed useless entity mixin

--- src/main/java/com/legacy/goodnightsleep/mixin/EntityMixin.java
+++ src/main/java/com/legacy/goodnightsleep/mixin/EntityMixin.java
@@ -1,17 +1,9 @@
 package com.legacy.goodnightsleep.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.horse.ZombieHorseEntity;
-
-@Mixin(Entity.class)
+//@Mixin(Entity.class)
 public class EntityMixin
 {
-	@Inject(at = @At("HEAD"), method = "getMountedYOffset()D", cancellable = true)
+	/*@Inject(at = @At("HEAD"), method = "getMountedYOffset()D", cancellable = true)
 	private void getMountedYOffset(CallbackInfoReturnable<Double> callback)
 	{
 		if (getThis() instanceof ZombieHorseEntity)
@@ -19,9 +11,9 @@ public class EntityMixin
 			callback.setReturnValue(getThis().getType().getSize().height * 0.75D - 0.2D);
 		}
 	}
-
+	
 	public Entity getThis()
 	{
 		return (Entity) (Object) this;
-	}
+	}*/
 }
